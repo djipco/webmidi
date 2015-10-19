cc57bd5d11be06a832346b161307acd9c8db8dc0
= <jp@cote.cc>
2015-10-18 20:28:30 -0400

    Updated doc.

--- npm/node_modules/grunt-contrib-yuidoc/node_modules/yuidocjs/tests/lib/testpreprocessormodule/testpreprocessormodule.js
+++
@@ -1,3 +0,0 @@
-module.exports=function(data) {
-	data.testModuleWasHere=true;
-}
--- npm/node_modules/grunt-release/node_modules/superagent/foo.js
+++
@@ -1,5 +0,0 @@
-var superagent = require('./');
-
-var request = superagent.get('/')
-request.set('foo', 'bar')
-//> r.set({foo: 'bar'})
