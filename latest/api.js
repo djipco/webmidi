4fc63e96c08f77615d2a0f940d07404e17865194
Jean-Philippe Côté <jp@djip.co>
2020-04-06 10:39:25 -0400

    Updates

---
+++ latest/api.js
@@ -0,0 +1,12 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Input",
+        "Output",
+        "WebMidi"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
