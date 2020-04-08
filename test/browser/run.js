2ca956b9eac5642adeb62703b37223a68db9cd86
Jean-Philippe Côté <jp@djip.co>
2020-04-08 18:08:54 -0400

    Start v3 work

---
+++ test/browser/run.js
@@ -0,0 +1,6 @@
+// Load all tests
+import "./WebMidi.test.js";
+
+// Run tests
+mocha.checkLeaks();
+mocha.run();
---
+++ test/browser/setup.js
@@ -0,0 +1,3 @@
+// Setup the testing environment. This must be done before loading in the tests. This is why we do
+// it in a separate file before running tests.
+mocha.setup("bdd");
