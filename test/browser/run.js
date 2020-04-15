279a408fec4b8f87de74c0bddba093f20c91c3c7
Jean-Philippe Côté <jp@djip.co>
2020-04-15 17:09:26 -0400

    Run multiple class tests

--- test/browser/run.js
+++ test/browser/run.js
@@ -1,5 +1,15 @@
 // Load all tests
-import "./WebMidi.test.js";
+import WebMidiTests from "./WebMidi.test.js"
+import NoteTests from "./Note.test.js"
+import OoutputTests from "./Output.test.js"
+
+describe("WebMidi.js Test Suite", function() {
+
+  WebMidiTests();
+  NoteTests();
+  // OoutputTests();
+
+});
 
 // Run tests
 mocha.checkLeaks();
