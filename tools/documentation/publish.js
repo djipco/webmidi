1b76a9d6f0643913f7ea6df1354d99622673c00a
Jean-Philippe Côté <jp@djip.co>
2020-05-09 16:27:01 -0400

    Commit and push to current branch

--- tools/documentation/publish.js
+++ tools/documentation/publish.js
@@ -1,5 +1,14 @@
-var ghpages = require("gh-pages");
+const moment = require("moment");
+const ghpages = require("gh-pages");
+const git = require("simple-git")();
 
-ghpages.publish("docs", err => {
+// Prepare commit message
+let message = "Updated on: " + moment().format();
+
+// Commit and push to current branch
+git.commit(message, ["docs"]).push();
+
+// Commit and push to GitHub pages
+ghpages.publish("docs", {message: message}, err => {
   if (err) console.error(err);
 });
