6be90fc4208d6993adf1082bf3d29b6836fcb91c
Jean-Philippe Côté <jp@djip.co>
2020-05-09 21:25:16 -0400

    Remove commit

--- scripts/documentation/publish.js
+++ scripts/documentation/publish.js
@@ -1,12 +1,8 @@
 const moment = require("moment");
 const ghpages = require("gh-pages");
-const git = require("simple-git")();
-
-// Prepare commit message
-let message = "Updated on: " + moment().format();
 
 // Commit and push to current branch
-git.commit(message, ["docs"]).push();
+let message = "Updated on: " + moment().format();
 
 // Commit and push to GitHub pages
 ghpages.publish("docs", {message: message}, err => {
