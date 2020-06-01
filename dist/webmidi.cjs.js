a5e37d8fa6382935dab69cf08727d6937a3eeac8
Jean-Philippe Côté <jp@djip.co>
2020-06-01 16:28:03 -0400

    Built on: 2020-06-01T16:28:03-04:00

--- dist/webmidi.cjs.js
+++ dist/webmidi.cjs.js
@@ -1454,6 +1454,41 @@ class Input extends e {
 
 }
 
+let validators = {};
+
+validators.controlChangeIdentifier = value => {
+  try {
+    validators.controlChangeName(value);
+    validators.controlChangeNumber(value);
+  } catch (err) {
+    throw new TypeError("Control change must be identified with a valid name or an integer between 0 and 199.");
+  }
+};
+
+validators.controlChangeName = value => {
+  if (wm.MIDI_CONTROL_CHANGE_MESSAGES[value] == undefined) {
+    throw new TypeError("Control change name does not exist.");
+  }
+};
+
+validators.controlChangeNumber = value => {
+  if (!Number.isInteger(value) || !(value >= 0 && value <= 119)) {
+    throw new TypeError("Control change number must be an integer between 0 and 119.");
+  }
+};
+
+validators.controlChangeValue = value => {
+  if (!Number.isInteger(value) || !(value >= 0 && value <= 127)) {
+    throw new TypeError("Control change value must be an integer between 0 and 127");
+  }
+};
+
+validators.options = value => {
+  if (typeof value === "object" && value !== null) {
+    throw new TypeError("Options must be an object");
+  }
+};
+
 function check(values, types) {
   // Make sure we are working with arrays
   values = Array.from(values);
@@ -1461,10 +1496,10 @@ function check(values, types) {
   console.log(values, types); // Execute all validators
 
   types.forEach((validator, index) => {
-    if (types[validator] === undefined) {
+    if (validators[validator] === undefined) {
       throw new TypeError(`Invalid validator (${validator})`);
     } else {
-      types[validator](values[index]);
+      validators[validator](values[index]);
     }
   }); // If we make it here, its all good!
 
