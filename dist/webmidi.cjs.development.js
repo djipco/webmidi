27a5337e248963c6fccfc20c5db4daa33f6ba118
Jean-Philippe Côté <jp@djip.co>
2020-06-02 12:20:00 -0400

    Built on: 2020-06-02T12:19:59-04:00

--- dist/webmidi.cjs.development.js
+++ dist/webmidi.cjs.development.js
@@ -1726,9 +1726,7 @@ class OutputChannel extends e {
       throw new TypeError("Control change must be identified with a valid name or an integer between 0 and 119.");
     }
 
-    console.log(controller, value);
-
-    if (!Number.isInteger(value) || !(value >= 0 && value <= 119)) {
+    if (!Number.isInteger(controller) || !(value >= 0 && value <= 119)) {
       throw new TypeError("Control change number must be an integer between 0 and 119.");
     }
 
