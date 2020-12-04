7f99a7453862b925f04f9f8c327f08a2ce4346fc
Jean-Philippe Côté <jp@djip.co>
2020-12-03 20:11:59 -0500

    Built on: 2020-12-03T20:11:59-05:00

--- dist/webmidi.esm.js
+++ dist/webmidi.esm.js
@@ -3,7 +3,7 @@
  * A JavaScript library to kickstart your MIDI projects
  * https://webmidijs.org
  *
- * This build was generated on August 17th 2020.
+ * This build was generated on December 3rd 2020.
  *
  *
  *
@@ -416,6 +416,8 @@ class InputChannel extends e {
       }
 
     }
+
+    return false;
 
   }
 
@@ -5967,21 +5969,15 @@ class WebMidi extends e {
     // If we are inside Node.js, polyfill navigator.requestMIDIAccess()
     if (this.isNode) {
 
-      // Important: performance must be imported before jzz because jzz checks for its existence at
-      // startup and falls back to something less precise if absent.
+      // Important: 'performance' must be imported before 'jzz' because 'jzz' checks for its
+      // existence at startup and falls back to something less precise if absent.
       // global.performance = require("perf_hooks").performance;
       global.navigator = require("jzz"); // THIS SHOULD BE TARGETED TO ONLY REQUESTMIDIACCESS !!!!
 
     }
 
-
-    console.warn("browser: ", typeof window !== "undefined" &&
-      typeof window.performance !== "undefined" &&
-      typeof window.performance.now === "function");
-
-
-    // Check if performance.now() is available. In a modern browser, it should be the case. In
-    // Node.js, we must require the perf_hooks module which is available in v8.5+.
+    // Check if performance.now() is available. In a modern browser, it should be. In Node.js, we
+    // must require the perf_hooks module which is available in v8.5+.
     if (
       typeof window !== "undefined" &&
       typeof window.performance !== "undefined" &&
