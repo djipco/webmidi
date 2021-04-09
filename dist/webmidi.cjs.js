8265ddcca14e1d11ece131dab02cbc56d6715a82
Jean-Philippe Côté <jp@djip.co>
2021-04-09 14:06:13 -0400

    Built on: 2021-04-09T14:06:13-04:00

--- dist/webmidi.cjs.js
+++ dist/webmidi.cjs.js
@@ -2462,8 +2462,8 @@ class OutputChannel extends e {
 
     if (options.duration > 0 && isFinite(String(options.duration).trim() || NaN)) {
       let noteOffOptions = {
-        // time: WebMidi.convertToTimestamp(options.time) + options.duration,
-        time: (wm.convertToTimestamp(options.time) || wm.time) + options.duration,
+        time: wm.convertToTimestamp(options.time) + options.duration,
+        // time: (WebMidi.convertToTimestamp(options.time) || WebMidi.time) + options.duration,
         release: options.release,
         rawRelease: options.rawRelease
       };
