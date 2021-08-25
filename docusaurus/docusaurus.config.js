4f9de07ac9c4ffa4a6a5db984089d5a1dde44d8d
Jean-Philippe Côté <jp@djip.co>
2021-08-25 18:37:48 -0400

    Fix base URL issue

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/webmidi/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
