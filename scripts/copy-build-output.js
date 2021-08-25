c8d27db348584b27841beb916656d0551a229e55
Jean-Philippe Côté <jp@djip.co>
2021-08-25 16:09:59 -0400

    Automate moving built files

--- scripts/copy-build-output.js
+++ scripts/copy-build-output.js
@@ -1,11 +1,28 @@
 // Modules
 const fs = require("fs-extra");
-const path = require("path");
-const os = require("os");
+const git = require("simple-git/promise")();
+const moment = require("moment");
+
+const SOURCE_PATH = "docusaurus/build/*";
+const TARGET_PATH = "."
+const TARGET_ELEMENTS = [
+  "assets",
+  "blog",
+  "docs",
+  "img",
+  "markdown-page",
+  ".nojekyll",
+  "404.html",
+  "index.html",
+  "sitemap.xml",
+]
 
 async function execute() {
-  await fs.copy("docusaurus/build/*",  ".", {overwrite: true});
-  log(`Switching from '${ORIGINAL_BRANCH}' branch to '${TARGET_BRANCH}' branch`);
+  await fs.copy(SOURCE_PATH,  TARGET_PATH, {overwrite: true});
+  log(`Copied files matching '${SOURCE_PATH}' to '${TARGET_PATH}'`);
+  await git.add(TARGET_ELEMENTS);
+  await git.commit("Built from Docusaurus and updated on: " + moment().format(), TARGET_ELEMENTS);
+  // await git.push();
 }
 
 // Execute and catch errors if any (in red)
