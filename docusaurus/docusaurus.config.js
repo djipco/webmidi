8e96d702fdda467c4889eba92c832b7b9f1f6c75
Jean-Philippe Côté <jp@djip.co>
2021-08-25 19:26:51 -0400

    Fix repo links

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -96,13 +96,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/djipco/webmidi/edit/master/docusaurus/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/djipco/webmidi/edit/master/docusaurus/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
