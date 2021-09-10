4d2a1af1adc0805a5389fa2a8ea2f3af2b7d1137
Jean-Philippe Côté <jp@djip.co>
2021-09-10 14:39:53 -0400

    Built on 2021-09-10T14:39:53-04:00

--- dist/webmidi.esm.js
+++ dist/webmidi.esm.js
@@ -2721,7 +2721,6 @@ class OutputChannel extends e {
     let o = {rawAttack: nVelocity};
 
     wm.getValidNoteArray(note, o).forEach(n => {
-      console.info(n);
       this.send(
         [
           (wm.MIDI_CHANNEL_VOICE_MESSAGES.noteon << 4) + (this.number - 1),
