4c7b2e502c5aa1f01582d2948c5bcc0f78d382f4
Jean-Philippe Côté <jp@djip.co>
2021-09-13 09:22:03 -0400

    Generated by TypeScript compiler on 2021-09-13T09:22:03-04:00

--- dist/webmidi.cjs.d.ts
+++ dist/webmidi.cjs.d.ts
@@ -1,37 +1,48 @@
 export var __esModule: boolean;
 /**
- * The `Note` class represents a single note to be played. The `Note` can be played on a single
- * channel by using [OutputChannel.playNote()]{@link OutputChannel#playNote} or on multiple
- * channels at once by using [Output.playNote()]{@link Output#playNote}.
+ * The `Note` class represents a single note such as `"D3"`, `"G#4"`, `"F-1"`, `"Gb7"`, etc. The
+ * actual MIDI note number associated with the note is determined when the note is played or
+ * received. This is because, the `octaveOffset` property can be used to offset the note number to
+ * match external devices where middle C is not equal to C4.
  *
- * If the note's `duration` property is set, the note will be stopped at the end of the duration. If
- * no duration is set, it will play until it is explicitly stopped using
- * [OutputChannel.stopNote()]{@link OutputChannel#stopNote} or
- * [Output.stopNote()]{@link Output#stopNote}.
+ * `Note` objects can be played back on a single channel by calling
+ * [OutputChannel.playNote()]{@link OutputChannel#playNote}. A note can also be played back on
+ * multiple channels of the same output by using [Output.playNote()]{@link Output#playNote}.
  *
- * @param value {string|number} The name or note number of the note to create. If a number is used,
- * it must be an integer between 0 and 127. If a string is used, it must be the note name followed
- * by the octave (`"C3"`, `"G#4"`, `"F-1"`, `"Db7"`, etc.). The octave range must be between -1 and
- * 9. The lowest note is C-1 (MIDI note number 0) and the highest note is G9 (MIDI note number 127).
+ * The note has attack and release velocities set at 0.5 by default. This can be changed by passing
+ * in the appropriate option. It is also possible to set a system-wide default for attack and
+ * release velocities by using the `WebMidi.defaults` property.
+ *
+ * The note also has a duration. Playback will be stopped (by sending **noteoff** sent) event the
+ * duration has elapsed. By default, the duration is set to `Infinity`. In this case, it will never
+ * stop playing unless explicitly stopped by calling a method such as
+ * OutputChannel.stopNote()]{@link OutputChannel#stopNote} or
+ * [Output.stopNote()]{@link Output#stopNote} or similar.
+ *
+ * @param value {string} The value used to create the note. If a string is used, it must be the note
+ * name followed by the octave (`"C3"`, `"G#4"`, `"F-1"`, `"Db7"`, etc.). If a number is used, it
+ * must be an integer between 0 and 127. When converting the number to a note name, middle C is
+ * considered to be C4 (note number 60).
  *
  * @param {Object} [options={}]
- *
- * @param {number} [options.duration=Infinity] The number of milliseconds before the note should be
- * explicitly stopped.
  *
  * @param {number} [options.attack=0.5] The note's attack velocity as a decimal number between 0 and
  * 1.
  *
- * @param {number} [options.octaveOffset=0] The offset to apply to the reported octave
+ * @param {number} [options.duration=Infinity] The number of milliseconds before the note should be
+ * explicitly stopped.
  *
- * @param {number} [options.release=0.5] The note's release velocity as a decimal number between 0
- * and 1.
+ * @param {number} [options.octaveOffset=0] An integer to offset the octave value. This is only used
+ * when the note is specified using a MIDI note number.
  *
  * @param {number} [options.rawAttack=64] The note's attack velocity as an integer between 0 and
  * 127.
  *
  * @param {number} [options.rawRelease=64] The note's release velocity as an integer between 0 and
  * 127.
+ *
+ * @param {number} [options.release=0.5] The note's release velocity as a decimal number between 0
+ * and 1.
  *
  * @throws {Error} Invalid note name.
  * @throws {Error} Invalid note number.
@@ -45,23 +56,6 @@ export var __esModule: boolean;
  */
 export class Note {
     constructor(value: any, options?: {});
-    set number(arg: number);
-    /**
-     * The MIDI note number as an integer between 0 and 127
-     * @type {number}
-     */
-    get number(): number;
-    set name(arg: string);
-    /**
-     * The name of the note with the octave number (`"C3"`, `"G#4"`, `"F-1"`, `"Db7"`, etc.).
-     *
-     * The name is affected by the `octaveOffset` property. For instance, a `Note` with a MIDI note
-     * number of 60 will be reported as `C4` if the `octaveOffset` property is `0`. However, it will
-     * be reported as `C5` if the  `octaveOffset` is `1`.
-     *
-     * @type {string}
-     */
-    get name(): string;
     set duration(arg: number);
     /**
      * The duration of the note as a positive decimal number representing the number of milliseconds
@@ -82,19 +76,7 @@ export class Note {
      * @type {number}
      */
     get release(): number;
-    set octaveOffset(arg: number);
-    /**
-     * An integer to offset the reported octave of the note. By default, middle C (MIDI note number
-     * 60) is placed on the 4th octave (C4).
-     *
-     * If, for example, `octaveOffset` is set to 2, MIDI note number 60 will be reported as C6. If
-     * `octaveOffset` is set to -1, MIDI note number 60 will be reported as C3.
-     *
-     * @type {number}
-     *
-     * @since 3.0
-     */
-    get octaveOffset(): number;
+    name: any;
     set rawAttack(arg: number);
     /**
      * The raw attack velocity of the note as an integer between 0 and 127.
@@ -107,19 +89,25 @@ export class Note {
      * @type {number}
      */
     get rawRelease(): number;
-    _number: number | false;
-    _octaveOffset: number;
-    _duration: number;
-    _rawAttack: number;
-    _rawRelease: number;
     /**
-     * The octave of the note as an integer between -1 and 9.
-     * @type {number}
+     * Returns the MIDI note number of the note (0-127). To calculate the MIDI note number, middle C
+     * is considered to be C4 (MIDI note number 60). The returned MIDI note number is offset by the
+     * value of the `octaveOffset` parameter (if any).
+     *
+     * @param {number} [octaveOffset=0] A integer to offset the octave by
+     *
+     * @returns {number|false} The MIDI note number (an integer between 0 and 127) or `false` (if the
+     * offset causes the note to fall outside the MIDI range).
+     *
+     * @since 3.0.0
      */
-    get octave(): number;
+    getNumber(octaveOffset?: number): number | false;
+    _rawAttack: number;
+    _duration: number;
+    _rawRelease: number;
 }
 declare const utils: Utilities;
-declare const wm: WebMidi;
+declare const wm: WebMidi$1;
 /**
  * The `Utilities` class contains all the general-purpose utility functions of the library. The
  * class is a singleton and is not meant to be instantiated. Its methods are static.
@@ -153,6 +141,19 @@ declare class Utilities {
      * name could not successfully be parsed to a number.
      */
     getNoteNumberByName(name: string, octaveOffset?: number): number | false;
+    /**
+     * Given a proper note name ("C#4", "Gb-1", etc.), this method returns an object containing the
+     * fragments composing it (uppercase letter, accidental and octave). If the name is invalid,
+     * `false` is returned.
+     *
+     * @param name
+     * @returns {{octave: number, letter: string, accidental: string}|false}
+     */
+    getNoteFragments(name: any): false | {
+        octave: number;
+        letter: string;
+        accidental: string;
+    };
     /**
      * Returns a sanitized array of valid MIDI channel numbers (1-16). The parameter should be a
      * single integer or an array of integers.
@@ -281,6 +282,13 @@ declare class Utilities {
         rawAttack?: number;
         rawRelease?: number;
     }): Note[];
+    /**
+     *
+     * @param {number}
+     * @param {octaveOffset}
+     * @returns {string}
+     */
+    getNoteNameByNumber(number: any, octaveOffset: any): string;
 }
 /**
  * The `WebMidi` object makes it easier to work with the Web MIDI API. Basically, it simplifies
@@ -308,7 +316,22 @@ declare class Utilities {
  *
  * @extends EventEmitter
  */
-declare class WebMidi {
+declare class WebMidi$1 {
+    /**
+     * Object containing system-wide default values that can be changed to customize how the library
+     * works.
+     *
+     * @type {Object}
+     *
+     * @property {object}  defaults.note - Default values relating to note
+     * @property {number}  defaults.note.attackVelocity - A number between 0 and 1 representing the
+     * default attack velocity of notes. Initial value is 0.5.
+     * @property {number}  defaults.note.releaseVelocity - A number between 0 and 1 representing the
+     * default release velocity of notes. Initial value is 0.5.
+     * @property {number}  defaults.note.duration - A number representing the default
+     * duration of notes (in seconds). Initial value is Infinity.
+     */
+    defaults: any;
     /**
      * The `MIDIAccess` instance used to talk to the Web MIDI API. This should not be used directly
      * unless you know what you are doing.
@@ -519,8 +542,7 @@ declare class WebMidi {
     private noteNameToNumber;
     /**
      * Returns the octave number for the specified MIDI note number (0-127). By default, the value is
-     * based on middle C (note number 60) being placed on the 4th octave (C4). However, by using the
-     * [octaveOffset]{@link WebMidi#octaveOffset} property, you can offset the result as desired.
+     * based on middle C (note number 60) being placed on the 4th octave (C4).
      *
      * **Note**: since v3.x, this method returns `false` instead of `undefined` when the value cannot
      * be parsed to a valid octave.
