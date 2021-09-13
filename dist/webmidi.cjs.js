882fe47cda91ccec1b96a1cdc0c436a69b6df3d9
Jean-Philippe Côté <jp@djip.co>
2021-09-13 09:24:55 -0400

    Built on 2021-09-13T09:24:55-04:00

--- dist/webmidi.cjs.js
+++ dist/webmidi.cjs.js
@@ -200,7 +200,7 @@ class Utilities {
     // let result = ((octave + 1 - octaveOffset) * 12) + semitone;
 
     const fragments = this.getNoteFragments(name);
-    let result = (fragments.octave + 1 - octaveOffset) * 12;
+    let result = (fragments.octave + 1 + octaveOffset) * 12;
     result += {
       C: 0,
       D: 2,
