cf51fdb57600f15c1b7d7606631488ffec7ef643
Jean-Philippe Côté <jp@djip.co>
2021-09-19 12:59:27 -0400

    Changed MailChimp code

--- website/static/scripts/mailchimp/newsletter-exit-prompt.js
+++ website/static/scripts/mailchimp/newsletter-exit-prompt.js
@@ -1,1 +1,1 @@
-!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/4ad018201643381a89d30000c/9d91c118d6ccf7d13db6caf87.js");
+!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/4ad018201643381a89d30000c/dd4defc34880d9e134204e0da.js");
