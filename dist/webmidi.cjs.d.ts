881d03f6de03b4ac5f6f39f9d86b67eadc7a0e88
Jean-Philippe Côté <jp@djip.co>
2021-09-20 09:49:48 -0400

    Generated by TypeScript compiler on 2021-09-20T09:49:48-04:00

--- dist/webmidi.cjs.d.ts
+++ dist/webmidi.cjs.d.ts
@@ -2,14 +2,6 @@ export var __esModule: boolean;
 /**
  * The `Note` class represents a single musical note such as `"D3"`, `"G#4"`, `"F-1"`, `"Gb7"`, etc.
  *
- * Note that a `Note` object does not have a MIDI number per se. The MIDI note number is determined
- * when the note is played. This is because, the `octaveOffset` property of various objects
- * (`WebMidi`, `InputChannel`, `Output`, etc.) can be used to offset the note number to match
- * external devices where middle C is not equal to C4.
- *
- * The octave of the note has no intrinsic limit. You can specify a note to be "F27" or "G#-16".
- * However, to play such notes on a MIDI channel, the channel will need to be offset accordingly.
- *
  * `Note` objects can be played back on a single channel by calling
  * [OutputChannel.playNote()]{@link OutputChannel#playNote}. A note can also be played back on the
  * multiple channels of an output by using [Output.playNote()]{@link Output#playNote}.
@@ -26,9 +18,9 @@ export var __esModule: boolean;
  *
  * @param value {string|number} The value used to create the note. If an identifier string is used,
  * it must be the note name (with optional accidental) followed by the octave (`"C3"`, `"G#4"`,
- * `"F-1"`, `"Db7"`, etc.). If a number is used, it must be an integer between 0 and 127. The number
- * will be converted to a note name. In this case, middle C is considered to be C4 (note number 60)
- * but that can be offset with the `octaveOffset`property.
+ * `"F-1"`, `"Db7"`, etc.). If a number is used, it must be an integer between 0 and 127. In this
+ * case, middle C is considered to be C4 (note number 60) but that can be offset with the
+ * `octaveOffset`property.
  *
  * @param {Object} [options={}]
  *
@@ -136,6 +128,14 @@ export class Note {
      * @since 3.0.0
      */
     get rawRelease(): number;
+    /**
+     * The MIDI number of the note. This number is derived from the note identifier using C4 as a
+     * reference for middle C.
+     *
+     * @type {number}
+     * @since 3.0.0
+     */
+    get number(): number;
 }
 declare const utils: Utilities;
 declare const wm: WebMidi;
