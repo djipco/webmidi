0b708c5db4771ac72b9a55a32dac63323346d754
Jean-Philippe Côté <jp@djip.co>
2021-09-22 08:10:49 -0400

    Built on 2021-09-22T08:10:49-04:00

--- dist/webmidi.cjs.js
+++ dist/webmidi.cjs.js
@@ -1138,6 +1138,7 @@ class InputChannel extends e {
     event.type = event.message.type || "unknownmidimessage";
     const data1 = e.message.dataBytes[0];
     const data2 = e.message.dataBytes[1];
+    console.log(event);
 
     if (event.type === "noteoff" || event.type === "noteon" && data2 === 0) {
       /**
