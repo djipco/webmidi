5bc073f01edc56680fb35b033510bfcbe88ad0d9
Jean-Philippe Côté <jp@djip.co>
2021-09-22 12:02:20 -0400

    Built on 2021-09-22T12:02:20-04:00

--- dist/webmidi.cjs.js
+++ dist/webmidi.cjs.js
@@ -2506,16 +2506,15 @@ class Input extends e {
       } // Validation
 
 
-      if ( // WebMidi.MIDI_CHANNEL_VOICE_MESSAGES[event] !== undefined &&
-      wm.CHANNEL_EVENTS.includes(event) && options.channels === undefined) {
+      if (wm.CHANNEL_EVENTS.includes(event) && options.channels === undefined) {
         throw new Error("For channel-specific events, 'options.channels' must be defined.");
       }
     }
 
     let listeners = []; // Check if the event is channel-specific or input-wide
+    // if (WebMidi.MIDI_CHANNEL_VOICE_MESSAGES[event] === undefined) {
 
-    if (wm.MIDI_CHANNEL_VOICE_MESSAGES[event] === undefined) {
-      // if (!WebMidi.CHANNEL_EVENTS.includes(event)) {
+    if (!wm.CHANNEL_EVENTS.includes(event)) {
       listeners.push(super.addListener(event, listener, options));
     } else {
       utils.sanitizeChannels(options.channels).forEach(ch => {
