c1fc946d9f63f9714fc9efd2a448e56b33577863
Jean-Philippe Côté <jp@djip.co>
2021-10-08 19:31:00 -0400

    Sort members

--- scripts/api-documentation/generate-markdown.js
+++ scripts/api-documentation/generate-markdown.js
@@ -1,3 +1,4 @@
+// Imports
 const fs = require("fs-extra");
 const fsPromises = require("fs").promises;
 const git = require("simple-git/promise")();
@@ -9,91 +10,125 @@ const replace = require("replace-in-file");
 const rimraf = require("@alexbinary/rimraf");
 const moment = require("moment");
 
+// Paths
 const ROOT_PATH = process.cwd();
 const TARGET_PATH = path.join(process.cwd(), "website", "api", "classes");
 const TEMPLATE_DIR = path.join(__dirname, "markdown-template");
 
+// Source files to document
+const FILES = [
+  path.join(ROOT_PATH, "node_modules", "djipevents", "src", "djipevents.js"),
+  path.join(ROOT_PATH, "src", "Enumerations.js"),
+  path.join(ROOT_PATH, "src", "Input.js"),
+  path.join(ROOT_PATH, "src", "InputChannel.js"),
+  path.join(ROOT_PATH, "src", "Message.js"),
+  path.join(ROOT_PATH, "src", "Note.js"),
+  path.join(ROOT_PATH, "src", "Output.js"),
+  path.join(ROOT_PATH, "src", "OutputChannel.js"),
+  path.join(ROOT_PATH, "src", "Utilities.js"),
+  path.join(ROOT_PATH, "src", "WebMidi.js")
+];
+
 async function generate() {
 
-  // Create temporary directory to host modified files
+  // Create temporary directory to store temporary file
   const tempPath = await fsPromises.mkdtemp(path.join(os.tmpdir(), "webmidi-api-markdown-"));
+  const tempFilePath = path.join(tempPath, "All.js");
 
-  const files = [
-    path.join(ROOT_PATH, "src", "Enumerations.js"),
-    path.join(ROOT_PATH, "src", "Input.js"),
-    path.join(ROOT_PATH, "src", "InputChannel.js"),
-    path.join(ROOT_PATH, "src", "Message.js"),
-    path.join(ROOT_PATH, "src", "Note.js"),
-    path.join(ROOT_PATH, "src", "Output.js"),
-    path.join(ROOT_PATH, "src", "OutputChannel.js"),
-    path.join(ROOT_PATH, "src", "Utilities.js"),
-    path.join(ROOT_PATH, "src", "WebMidi.js"),
+  // Combine all source files into a single document (this is needed because jsdoc2md does not
+  // resolve parents accross files)
+  FILES.forEach(filepath => {
+    fs.outputFileSync(tempFilePath, fs.readFileSync(filepath), {flag: "a"});
+  });
 
-    // path.join(ROOT_PATH, "src", "EventEmitter.js"),
-    // path.join(ROOT_PATH, "src", "Listener.js"),
+  // Strip all import statements from the combined file
+  const options = {
+    files: tempFilePath,
+    from: [/^import .*\n/gm, /^export class /gm], // strip out import and export
+    to: ["", "class "]
+  };
+  await replace(options);
 
-    // path.join(ROOT_PATH, "scripts", "api-documentation", "djipevents", "EventEmitter.js"),
-    // path.join(ROOT_PATH, "scripts", "api-documentation", "djipevents", "Listener.js")
-    // path.join(ROOT_PATH, "node_modules", "djipevents", "src", "djipevents.js")
-  ];
+  // Get template data by parsing the file with all classes
+  const templateData = jsdoc2md.getTemplateDataSync({files: tempFilePath});
 
-  // Get list of source files
-  // let files = await fsPromises.readdir(SOURCE_PATH);
 
-  // Copy source files to temp dir, strip out the import/export statements and create markdown file
-  await files.forEach(async filepath => {
 
-    const file = path.basename(filepath);
+  // Sort data according to kind (member, function, event) and then according to name
+  const order = {class: 0, constructor: 1, function: 2, member: 3, event: 4};
+  templateData.sort((a, b) => {
 
-    // await fs.copy(path.join(SOURCE_PATH, file), path.join(tempPath, file), {overwrite: true});
-    await fs.copy(filepath, path.join(tempPath, file), {overwrite: true});
+    // if (order[a.kind] === order[b.kind]) {
+    //   return a.id.localeCompare(b.id);
+    // } else if (order[a.kind] < order[b.kind]) {
+    //   return -1;
+    // } else {
+    //   return 1;
+    // }
 
-    const options = {
-      files: path.join(tempPath, file),
-      from: [/^import .*\n/gm, /^export class /gm], // strip out import and export
-      to: ["", "class "]
-    };
-    await replace(options);
-
-    // Generate markdown from JSDoc comments
-    const markdown = jsdoc2md.renderSync({
-      files: path.join(tempPath, file),
-      partial: [
-        path.join(TEMPLATE_DIR, "all-docs", "docs", "header.hbs"),
-        path.join(TEMPLATE_DIR, "all-docs", "docs", "body", "body.hbs"),
-        path.join(TEMPLATE_DIR, "all-docs", "docs", "member-index", "member-index-grouped.hbs"),
-      ],
-      noCache: true,
-      "heading-depth": 1,
-
-      // Put <hr> breaks between identifiers
-      separators: true,
-
-      // Format identifiers as code (wrap function/property/class/etc. names in backticks).
-      "name-format": false,
-
-      // "module-index-format": "dl", // none, grouped, table, dl
-      // "global-index-format": "none" // none, grouped, table, dl
-      "param-list-format": "table", // 'list' or 'table' (default)
-      "property-list-format": "table", // 'list' or 'table' (default)
-      "member-index-format": "grouped" // grouped, list
-    });
-
-    // Write the markdown file
-    let filename = path.basename(file, ".js");
-    fs.writeFileSync(path.join(TARGET_PATH, `${filename}.md`), markdown);
-    console.info(`Saved markdown file to ${filename}.md`);
-
-    // Commit generated files
-    await git.add([path.join(TARGET_PATH, `${filename}.md`)]);
-    await git.commit("Automatically generated on: " + moment().format(), [TARGET_PATH]);
-    await git.push();
-    console.info(`File committed to git: ${filename}.md`);
-
-    // Remove temporary file
-    await rimraf(path.join(tempPath, file));
+    if (order[a.kind] === order[b.kind]) {
+      return a.id.localeCompare(b.id);
+    } else {
+      return order[a.kind] < order[b.kind] ? -1 : 1;
+    }
 
   });
+
+
+  // fs.writeJsonSync(TARGET_PATH + "test.json", templateData);
+
+
+  // Build an array of class names (to output one  file per class)
+  const classNames = templateData.reduce((classNames, identifier) => {
+    if (identifier.kind === "class") classNames.push(identifier.name);
+    return classNames;
+  }, []);
+
+  // Define rendering options
+  const renderingOptions = {
+    data: templateData,
+
+    partial: [
+      path.join(TEMPLATE_DIR, "all-docs", "docs", "header.hbs"),
+      path.join(TEMPLATE_DIR, "all-docs", "docs", "body", "body.hbs"),
+      path.join(TEMPLATE_DIR, "all-docs", "docs", "member-index", "member-index-grouped.hbs"),
+    ],
+    noCache: true,
+    "heading-depth": 1,
+
+    separators: true, // Put <hr> breaks between identifiers
+
+    // Format identifiers as code (wrap function/property/class/etc. names in backticks).
+    "name-format": false,
+
+    // "module-index-format": "dl", // none, grouped, table, dl
+    // "global-index-format": "none" // none, grouped, table, dl
+    "param-list-format": "table", // 'list' or 'table' (default)
+    "property-list-format": "table", // 'list' or 'table' (default)
+    "member-index-format": "grouped" // grouped, list
+  };
+
+  // Create a separate documentation file for each class
+  for (const className of classNames) {
+
+    // Do not create files for EventEmitter and Listener
+    if (className === "EventEmitter" || className === "Listener") continue;
+
+    renderingOptions.template = `{{#class name="${className}"}}{{>docs}}{{/class}}`;
+    const output = jsdoc2md.renderSync(renderingOptions);
+    console.info(`Saved markdown file to ${className}.md`);
+    fs.writeFileSync(path.resolve(TARGET_PATH, `${className}.md`), output);
+    await git.add([path.join(TARGET_PATH, `${className}.md`)]);
+  }
+
+  // Commit generated files
+  await git.commit("Automatically generated on: " + moment().format(), [TARGET_PATH]);
+  console.info(`Files in ${TARGET_PATH} committed to git`);
+  await git.push();
+  console.info(`Files pushed to remote`);
+
+  // Remove temporary directory
+  await rimraf(tempPath);
 
 }
 
