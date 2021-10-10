ef42bd31cc00e36b7392dd9fa3aee09b00e134ac
Jean-Philippe Côté <jp@djip.co>
2021-10-10 14:01:59 -0400

    Improve documentation markdown generation

--- scripts/api-documentation/generate-markdown.js
+++ scripts/api-documentation/generate-markdown.js
@@ -11,39 +11,60 @@ const jsdoc2md = require("jsdoc-to-markdown");
 const moment = require("moment");
 const Handlebars = require("handlebars");
 
-
 // Paths
 const ROOT_PATH = process.cwd();
+const SOURCE_DIR = path.resolve(ROOT_PATH, "src");
 const TARGET_PATH = path.join(process.cwd(), "website", "api", "classes");
-// const TEMPLATE_DIR = path.join(__dirname, "markdown-template");
 const TEMPLATE_DIR = path.resolve(ROOT_PATH, "scripts/api-documentation/templates/");
-
-// Source files to document
-const FILES = [
-  path.resolve(ROOT_PATH, "node_modules/djipevents/src/djipevents.js"),
-  path.resolve(ROOT_PATH, "src/Enumerations.js"),
-  path.resolve(ROOT_PATH, "src/Input.js"),
-  path.resolve(ROOT_PATH, "src/InputChannel.js"),
-  path.resolve(ROOT_PATH, "src/Message.js"),
-  path.resolve(ROOT_PATH, "src/Note.js"),
-  path.resolve(ROOT_PATH, "src/Output.js"),
-  path.resolve(ROOT_PATH, "src/OutputChannel.js"),
-  path.resolve(ROOT_PATH, "src/Utilities.js"),
-  path.resolve(ROOT_PATH, "src/WebMidi.js")
-];
+const DJIPEVENTS = path.resolve(ROOT_PATH, "node_modules/djipevents/src/djipevents.js");
 
 async function generate() {
 
-  // Register some helpers
-  Handlebars.registerHelper("getEventName", string => string.split(":")[1]);
+  // Get source files list
+  let files = await fs.readdir(SOURCE_DIR);
+  files = files.map(file => path.resolve(SOURCE_DIR, file));
+  files.push(DJIPEVENTS);
 
-  FILES.forEach(filepath => {
+  // Register some helpers
+  Handlebars.registerHelper("eventName", string => string.split(":")[1]);
+  Handlebars.registerHelper("stripNewlines", stripNewlines);
+
+  Handlebars.registerHelper({
+    eq: (v1, v2) => v1 === v2,
+    ne: (v1, v2) => v1 !== v2,
+    lt: (v1, v2) => v1 < v2,
+    gt: (v1, v2) => v1 > v2,
+    lte: (v1, v2) => v1 <= v2,
+    gte: (v1, v2) => v1 >= v2,
+    and() {
+      return Array.prototype.every.call(arguments, Boolean);
+    },
+    or() {
+      return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+    }
+  });
+
+  // Register partials
+  // let partialFile = path.resolve(TEMPLATE_DIR, "partials/params.hbs");
+  // let partial = Handlebars.compile(fs.readFileSync(partialFile, "utf-8"));
+  // Handlebars.registerPartial("params", partial);
+  //
+  // partialFile = path.resolve(TEMPLATE_DIR, "partials/properties.hbs");
+  // partial = Handlebars.compile(fs.readFileSync(partialFile, "utf-8"));
+  // Handlebars.registerPartial("properties", partial);
+
+  files.forEach(filepath => {
+
+    // Parse file and gather output
     const output = parseFile(filepath);
+
+    // Save markdown files (exceptn for djipevents)
     const basename = path.basename(filepath, ".js");
-    if (basename !== "djipevents") {
+    if (basename !== path.basename(DJIPEVENTS, ".js")) {
       fs.writeFileSync(path.resolve(TARGET_PATH, `${basename}.md`), output);
       console.info(`Saved markdown file to ${basename}.md`);
     }
+
   });
 
   // Commit generated files
@@ -75,32 +96,40 @@ function parseFile(filepath) {
 
   // Class
   filtered = data.filter(el => el.kind === "class")[0];
-  hbs = fs.readFileSync(path.resolve(TEMPLATE_DIR, `class.hbs`), {encoding: "utf-8"});
+  hbs = fs.readFileSync(path.resolve(TEMPLATE_DIR, `core/class.hbs`), {encoding: "utf-8"});
   output += Handlebars.compile(hbs)(filtered);
+
 
   // Constructor
   filtered = data.filter(el => el.kind === "constructor")[0];
-  hbs = fs.readFileSync(path.resolve(TEMPLATE_DIR, `constructor.hbs`), {encoding: "utf-8"});
+  hbs = fs.readFileSync(path.resolve(TEMPLATE_DIR, `core/constructor.hbs`), {encoding: "utf-8"});
   output += Handlebars.compile(hbs)(filtered);
 
-  // Properties
+  // Members
   filtered = data.filter(el => el.kind === "member" && el.access !== "private");
-  hbs = fs.readFileSync(path.resolve(TEMPLATE_DIR, `properties.hbs`), {encoding: "utf-8"});
+  hbs = fs.readFileSync(path.resolve(TEMPLATE_DIR, `core/members.hbs`), {encoding: "utf-8"});
   output += Handlebars.compile(hbs)(filtered);
 
   // Methods
   filtered = data.filter(el => el.kind === "function" && el.access !== "private");
-  hbs = fs.readFileSync(path.resolve(TEMPLATE_DIR, `methods.hbs`), {encoding: "utf-8"});
+  hbs = fs.readFileSync(path.resolve(TEMPLATE_DIR, `core/methods.hbs`), {encoding: "utf-8"});
   output += Handlebars.compile(hbs)(filtered);
+
+  // console.log(filtered.exceptions);
+  filtered.forEach(item => {
+    if (item.exceptions) {
+      item.exceptions.forEach(exception => console.log(exception.type));
+    }
+  });
 
   // Events
   filtered = data.filter(el => el.kind === "event" && el.access !== "private");
-  hbs = fs.readFileSync(path.resolve(TEMPLATE_DIR, `events.hbs`), {encoding: "utf-8"});
+  hbs = fs.readFileSync(path.resolve(TEMPLATE_DIR, `core/events.hbs`), {encoding: "utf-8"});
   output += Handlebars.compile(hbs)(filtered);
 
   // Enums
   filtered = data.filter(el => el.kind === "enum" && el.access !== "private");
-  hbs = fs.readFileSync(path.resolve(TEMPLATE_DIR, `enumerations.hbs`), {encoding: "utf-8"});
+  hbs = fs.readFileSync(path.resolve(TEMPLATE_DIR, `core/enumerations.hbs`), {encoding: "utf-8"});
   output += Handlebars.compile(hbs)(filtered);
 
   return output;
@@ -110,93 +139,151 @@ function parseFile(filepath) {
 generate().catch(error => console.error("\x1b[31m", "Error: " + error, "\x1b[0m"));
 
 
-// async function generate() {
+
+
+
+
+
+function stripNewlines (input) {
+  if (input) return input.replace(/[\r\n]+/g, " ");
+}
+
 //
-//   // Create temporary directory to store the temporary input file
-//   const tempPath = await fsPromises.mkdtemp(path.join(os.tmpdir(), "webmidi-api-markdown-"));
-//   const tempFilePath = path.join(tempPath, "All.js");
+// function tableHead () {
 //
-//   // Combine all source files into a single document (this is needed because jsdoc2md does not
-//   // resolve parents across files)
-//   FILES.forEach(filepath => {
-//     fs.outputFileSync(tempFilePath, fs.readFileSync(filepath), {flag: "a"});
+//   var args = arrayify(arguments);
+//   var data = args.shift();
+//   if (!data) return;
+//   args.pop();
+//   var cols = args;
+//   var colHeaders = cols.map(function (col) {
+//     var spl = col.split("|");
+//     return spl[1] || spl[0];
+//   });
+//   cols = cols.map(function (col) {
+//     return col.split("|")[0];
+//   });
+//   var toSplice = [];
+//   cols = cols.filter(function (col, index) {
+//     var hasValue = data.some(function (row) {
+//       return typeof row[col] !== "undefined";
+//     });
+//     if (!hasValue) toSplice.push(index);
+//     return hasValue;
+//   });
+//   toSplice.reverse().forEach(function (index) {
+//     colHeaders.splice(index, 1);
 //   });
 //
-//   // Strip all import statements from the combined file and convert 'export class' to simply 'class'
-//   const options = {
-//     files: tempFilePath,
-//     from: [/^import .*\n/gm, /^export class /gm], // strip out import and export
-//     to: ["", "class "]
-//   };
-//   await replace(options);
+//   var table = "| " + colHeaders.join(" | ") + " |\n";
+//   table += cols.reduce(function (p) { return p + " --- |"; }, "|") + '\n';
+//   return table;
+// }
 //
-//   // Get jsdoc JSON data by parsing the file containing all merged classes
-//   const data = jsdoc2md.getTemplateDataSync({files: tempFilePath});
+// function tableRow () {
+//   var args = arrayify(arguments)
+//   var rows = args.shift()
+//   if (!rows) return
+//   var options = args.pop()
+//   var cols = args
+//   var output = ''
 //
-//   // Sort elements according to kind and then according to name
-//   // typdef!!!
-//   const order = {class: 0, constructor: 1, function: 2, enum: 3, member: 4, event: 5};
-//   data.sort((a, b) => {
+//   if (options.data) {
+//     var data = Handlebars.createFrame(options.data)
+//     cols.forEach(function (col, index) {
+//       var colNumber = index + 1
+//       data['col' + colNumber] = containsData(rows, col)
+//     })
+//   }
+//   rows.forEach(function (row) {
+//     output += options.fn(row, { data: data })
+//   })
+//   return output
+// }
 //
-//     if (order[a.kind] === order[b.kind]) {
-//       return a.id.localeCompare(b.id);
-//     } else {
-//       return order[a.kind] < order[b.kind] ? -1 : 1;
-//     }
+// function containsData (rows, col) {
+//   return rows.some(function (row) {
+//     return typeof row[col] !== 'undefined'
+//   })
+// }
 //
-//   });
+// function link (longname, options) {
+//   return options.fn(_link(longname, options))
+// }
 //
-//   // Build an array of class names (to output one file per class)
-//   const classNames = data.reduce((classNames, identifier) => {
-//     if (identifier.kind === "class") classNames.push(identifier.name);
-//     return classNames;
-//   }, []);
+// function _link (input, options) {
+//   if (typeof input !== 'string') return null
 //
-//   // Define rendering options
-//   const renderingOptions = {
+//   var linked, matches, namepath
+//   var output = {}
 //
-//     data: data,
-//
-//     // template: path.resolve(TEMPLATE_DIR, "main.hbs"),
-//
-//     // partial: [
-//     //   path.join(TEMPLATE_DIR, "all-docs", "docs", "header.hbs"),
-//     //   path.join(TEMPLATE_DIR, "all-docs", "docs", "body", "body.hbs"),
-//     //   path.join(TEMPLATE_DIR, "all-docs", "docs", "member-index", "member-index-grouped.hbs"),
-//     // ],
-//
-//     noCache: true,
-//     "heading-depth": 1,
-//
-//     separators: true, // Put <hr> breaks between identifiers
-//
-//     // Format identifiers as code (wrap function/property/class/etc. names in backticks).
-//     "name-format": false,
-//
-//   };
-//
-//   // Create a separate documentation file for each class
-//   for (const className of classNames) {
-//
-//     // Do not create files for EventEmitter and Listener
-//     if (className === "EventEmitter" || className === "Listener") continue;
-//
-//     renderingOptions.template = `{{#class name="${className}"}}{{>docs}}{{/class}}`;
-//     const output = jsdoc2md.renderSync(renderingOptions);
-//     console.info(`Saved markdown file to ${className}.md`);
-//     fs.writeFileSync(path.resolve(TARGET_PATH, `${className}.md`), output);
-//     await git.add([path.join(TARGET_PATH, `${className}.md`)]);
-//
+//   /*
+//    test input for
+//    1. A type expression containing a namepath, e.g. Array.<module:Something>
+//    2. a namepath referencing an `id`
+//    3. a namepath referencing a `longname`
+//    */
+//   if ((matches = input.match(/.*?<(.*?)>/))) {
+//     namepath = matches[1]
+//   } else {
+//     namepath = input
 //   }
 //
-//   // Commit generated files
-//   await git.commit("Automatically generated on: " + moment().format(), [TARGET_PATH]);
-//   console.info(`Files in ${TARGET_PATH} committed to git`);
-//   await git.push();
-//   console.info(`Files pushed to remote`);
-//
-//   // Remove temporary directory
-//   await rimraf(tempPath);
-//
+//   options.hash = { id: namepath }
+//   linked = _identifier(options)
+//   if (!linked) {
+//     options.hash = { longname: namepath }
+//     linked = _identifier(options)
+//   }
+//   if (!linked) {
+//     output = { name: input, url: null }
+//   } else {
+//     output.name = input.replace(namepath, linked.name)
+//     if (isExternal.call(linked)) {
+//       if (linked.description) {
+//         output.url = '#' + anchorName.call(linked, options)
+//       } else {
+//         if (linked.see && linked.see.length) {
+//           var firstLink = parseLink(linked.see[0])[0]
+//           output.url = firstLink ? firstLink.url : linked.see[0]
+//         } else {
+//           output.url = null
+//         }
+//       }
+//     } else {
+//       output.url = '#' + anchorName.call(linked, options)
+//     }
+//   }
+//   return output
 // }
-// generate().catch(error => console.error("\x1b[31m", "Error: " + error, "\x1b[0m"));
+//
+// function _identifier (options) {
+//   return _identifiers(options)[0]
+// }
+//
+// function _identifiers (options) {
+//   var query = {}
+//
+//   for (var prop in options.hash) {
+//     if (/^-/.test(prop)) {
+//       query[prop.replace(/^-/, '!')] = options.hash[prop]
+//     } else if (/^_/.test(prop)) {
+//       query[prop.replace(/^_/, '')] = new RegExp(options.hash[prop])
+//     } else {
+//       query[prop] = options.hash[prop]
+//     }
+//   }
+//   return arrayify(options.data.root).filter(where(query)).filter(function (doclet) {
+//     return !doclet.ignore && (state.options.private ? true : doclet.access !== 'private')
+//   })
+// }
+//
+// function escape (input) {
+//   if (typeof input !== 'string') return null
+//   return input.replace(/([\*|_])/g, '\\$1')
+// }
+//
+//
+// function equal (arg1, arg2) {
+//   return arg1 === arg2
+// }
