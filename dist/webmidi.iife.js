4fda18b65b7c0901413f0b6c855f4af483b6a611
Jean-Philippe Côté <jp@djip.co>
2021-10-13 09:53:59 -0400

    Built on 2021-10-13T09:53:59-04:00

--- dist/webmidi.iife.js
+++ dist/webmidi.iife.js
@@ -7660,7 +7660,7 @@
 
 
     _onInterfaceStateChange(e) {
-      console.log(e);
+      console.log("statechange", e.port.name, e.port.type, e.port.state);
 
       this._updateInputsAndOutputs();
       /**
