c2f521299684fedae1b3bf61341ea21a1767b887
Jean-Philippe Côté <jp@djip.co>
2021-10-13 12:32:48 -0400

    Built on 2021-10-13T12:32:48-04:00

--- dist/webmidi.cjs.js
+++ dist/webmidi.cjs.js
@@ -7906,7 +7906,7 @@ class WebMidi extends e {
       const current = this._outputs[i];
       const outputs = Array.from(this.interface.outputs.values());
 
-      if (!outputs.find(input => input === current._midiOutput)) {
+      if (!outputs.find(output => output === current._midiOutput)) {
         current.destroy();
 
         this._outputs.splice(i, 1);
