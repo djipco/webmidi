27833728769bef0aae5df580514ddf1d454f0ff3
J-MG21 <62517093+J-MG21@users.noreply.github.com>
2021-11-02 19:14:57 -0400

    Adding component button

---
+++ website/src/components/Button.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./Button.module.scss";
+
+/*export default function Button({children, type, href, target}) {
+  return (
+    <div className={`${styles.button} ${type}`} href={href} target={target}>
+      <a href="#">{children}</a>
+      <div className="button--bg"></div>
+    </div>
+  );
+}*/
+
+export default function Button(props) {
+  const component = "Button";
+  const{
+    componentClass,
+    children,
+    href,
+    type,
+    target,
+  } = props;
+  return (
+    <div
+      className={`
+        ${component}
+        ${styles.button}
+        ${type}_src-components-${component}-module
+        ${componentClass}
+      `}
+      href={href}
+      target={target}
+    >
+      <a href="#">{children}</a>
+      <div className={`${styles.buttonBg}`} />
+    </div>
+  );
+}
