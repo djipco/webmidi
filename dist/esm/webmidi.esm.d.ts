903177b4a9b935090f3607fb43755498902b8ec0
Jean-Philippe Côté <jp@djip.co>
2021-11-11 20:49:06 -0500

    Generated by TypeScript compiler on 2021-11-11T20:49:06-05:00

--- dist/esm/webmidi.esm.d.ts
+++ dist/esm/webmidi.esm.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for WebMidi.js 3.0.0-alpha.23
+// Type definitions for WebMidi.js 3.0.0-alpha.24
 // Project: https://webmidijs.org
 // Definitions by: Jean-Philippe Côté <https://github.com/djipco/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
