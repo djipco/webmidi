237e7538eb6ac87b3724ed473723aa515bddf562
Jean-Philippe Côté <jp@djip.co>
2021-11-11 20:38:29 -0500

    Built on 2021-11-11T20:38:29-05:00

--- dist/cjs/webmidi.cjs.js
+++ dist/cjs/webmidi.cjs.js
@@ -17,11 +17,28 @@
  * the License.
  */
 
-/* Version: 3.0.0-alpha.23 - November 11, 2021 10:02:15 */
+/* Version: 3.0.0-alpha.23 - November 11, 2021 20:38:29 */
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
+/**
+ * djipevents v2.0.1
+ * https://github.com/djipco/djipevents
+ * Build generated on November 10th, 2021.
+ *
+ * © Copyright 2019-2021, Jean-Philippe Côté.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 class e {
   constructor(e = !1) {
     this.eventMap = {}, this.eventsSuspended = 1 == e;
