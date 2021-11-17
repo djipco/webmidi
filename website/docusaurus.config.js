ca9ef521ffd60a2f0a10c1c4fab81998ad7c518a
J-MG21 <62517093+J-MG21@users.noreply.github.com>
2021-11-16 21:06:21 -0500

    In the front-page:  -Put out the DOCS btn  -Correction the What is WEBMIDI.js title  -Change the text in the presentation  -Change the size for the h1  -Change the illustration for the one on Twitter
     In footer:
     -Change the size for the sponsor
     -Add a link on the sponsor

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -25,7 +25,6 @@ module.exports = {
       id: "mcjs"
     }
   ],
-
   themeConfig: {
     navbar: {
       // title: "My Site",
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-//import clsx from "clsx";
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
@@ -7,26 +6,17 @@ import Button from "../components/Button";
 import Column from "../components/Column";
 import InformationBar from "../components/InformationBar";
 
-/*
-const textContent = {
-  presentation: `
-The <strong>Web MIDI API</strong> is a really exciting
-addition to the web platform that allows web developers to
-interact with <strong>MIDI musical instruments</strong> and devices.
-  `,
-
-}
-*/
-
-
 function HomepageHero() {
   const {siteConfig} = useDocusaurusContext();
+
   return (
     <section className="hero">
       <div className="container">
 
         <div className="texts">
-          <h1>{siteConfig.title}</h1>
+          <div className="logo">
+
+          </div>
           <span>{siteConfig.tagline}</span>
           <div className="cta">
             <Button
@@ -34,12 +24,6 @@ function HomepageHero() {
               href="./docs/getting-started"
               target="_self"
             >Get started - 5 minutes!
-            </Button>
-            <Button
-              type="button-bg-empty"
-              href="./docs"
-              target="_self"
-            >DOCS
             </Button>
           </div>
         </div>
@@ -53,35 +37,32 @@ function Presentation() {
   return (
     <section className={"presentation"}>
       <div className="container">
-        <h2>Whats is {siteConfig.title}</h2>
+        <h2>What is {siteConfig.title}?</h2>
         <Column
           type="col-2"
         >
           <p>
-            The <strong>Web MIDI API</strong> is a really exciting
-            addition to the web platform that allows web developers to
-            interact with <strong>MIDI musical instruments</strong> and devices.
+            The <strong>Web MIDI API</strong> is a really exciting addition to the web platform
+            allowing a web page to interact with <strong>MIDI musical instruments</strong>.
+            However, while great, most developers will find the API to be
+            too <em>low-level</em> for their needs. Having to perform binary arithmetic
+            or needing to read the 300-page MIDI spec is no fun (trust us on this!).
+            The goal behind <strong>WEBMIDI.js</strong> is to get you started with your web-based MIDI
+            project as efficiently as possible.
           </p>
 
           <div className="media">
             <img
-              src={useBaseUrl('img/front-page/presentation-illustration-keyboard-dark.png')}
-              alt="A keyboard illustration"
+              src={useBaseUrl('img/front-page/webmidi-demonstration.svg')}
+              alt="Demonstration of the WEBMIDI.js library"
             />
           </div>
         </Column>
+
       </div>
-        <div className="illustration">
-          <div className="container">
-            <img
-              src={useBaseUrl('img/front-page/webmidi-demonstration.svg')}
-              alt="Demonstration of the WEBMIDI.js library"/>
-          </div>
-        </div>
     </section>
   );
 }
-
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
