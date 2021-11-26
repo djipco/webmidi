74399b5fc1072623ab9fa9c2478ad9e3bea72b7a
Jean-Philippe Côté <jp@djip.co>
2021-11-26 14:29:41 -0500

    Add examples

--- website/static/scripts/mailchimp/newsletter-exit-prompt.js
+++ website/static/scripts/mailchimp/newsletter-exit-prompt.js
@@ -1,1 +1,1 @@
-!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/4ad018201643381a89d30000c/dd4defc34880d9e134204e0da.js");
+!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/4ad018201643381a89d30000c/0e8ce2085fb209fed1b656a74.js");
