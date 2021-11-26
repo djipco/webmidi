fe3dfaba7f02f0646955888f5ee34d444b1dcf27
Jean-Philippe Côté <jp@djip.co>
2021-11-25 23:17:25 -0500

    Generated by TypeScript compiler on 2021-11-25T23:17:25-05:00

--- dist/cjs/webmidi.cjs.d.ts
+++ dist/cjs/webmidi.cjs.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for WebMidi.js 3.0.0-alpha.26
+// Type definitions for WebMidi.js 3.0.0
 // Project: https://webmidijs.org
 // Definitions by: Jean-Philippe Côté <https://github.com/djipco/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
