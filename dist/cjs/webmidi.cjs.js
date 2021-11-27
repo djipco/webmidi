2461af52660a1dc9f8a21996c950fcc18af1fbf8
Jean-Philippe Côté <jp@djip.co>
2021-11-27 12:38:02 -0500

    Built on 2021-11-27T12:38:02-05:00

--- dist/cjs/webmidi.cjs.js
+++ dist/cjs/webmidi.cjs.js
@@ -1,5 +1,5 @@
 /**
- * WebMidi.js v3.0.1
+ * WebMidi.js v3.0.2
  * A JavaScript library to kickstart your MIDI projects
  * https://webmidijs.org
  * Build generated on November 27th, 2021.
@@ -17,7 +17,7 @@
  * the License.
  */
 
-/* Version: 3.0.1 - November 27, 2021 12:25:17 */
+/* Version: 3.0.2 - November 27, 2021 12:38:02 */
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
@@ -8418,6 +8418,8 @@ class WebMidi extends e {
      * @property {string} type `portschanged`
      * @property {Input|Output} target The [`Input`](Input) or [`Output`](Output) object that
      * triggered the event.
+     *
+     * @since 3.0.2
      */
 
     /**
@@ -8742,7 +8744,7 @@ class WebMidi extends e {
 
 
   get version() {
-    return "3.0.1";
+    return "3.0.2";
   }
   /**
    * @private
