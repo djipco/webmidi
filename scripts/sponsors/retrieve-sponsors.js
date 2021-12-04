b1dd3f0519891f2cb8884d9cb1c4b9aba521ade4
Jean-Philippe Côté <jp@djip.co>
2021-12-03 23:18:36 -0500

    Add "Sponsors" page

--- scripts/sponsors/retrieve-sponsors.js
+++ scripts/sponsors/retrieve-sponsors.js
@@ -1,33 +1,78 @@
 const { graphql } = require("@octokit/graphql");
 const { token } = require("../../.credentials/sponsors.js");
+const replace = require("replace-in-file");
+const path = require("path");
 
-console.log(token);
+const TARGET = path.join(process.cwd(), "website", "src", "pages", "sponsors", "index.md");
 
-async function test() {
+async function getSponsors() {
 
-  const { repository } = await graphql(
-    `
-    {
-      repository(owner: "djipco", name: "webmidi") {
-        issues(last: 3) {
-          edges {
-            node {
-              title
+  const query = `{
+
+    user (login: "djipco") {
+      sponsorshipsAsMaintainer(first: 100, includePrivate: true) {
+        totalCount
+        nodes {
+          sponsorEntity {
+          ... on User {
+            login
+            name
+            avatarUrl
+            url
+            sponsorshipForViewerAsSponsorable {
+                isOneTimePayment
+                privacyLevel
+                tier {
+                  id
+                  name
+                  monthlyPriceInDollars
+                }
+              }
             }
           }
         }
       }
     }
-  `,
-    {
-      headers: {
-        authorization: `token ${token}`,
-      },
-    }
+
+  }`;
+
+  const {user} = await graphql(
+    query,
+    { headers: { authorization: `token ${token}` } }
   );
 
-  return repository;
+  return user.sponsorshipsAsMaintainer.nodes;
 
 }
 
-test().then(data => console.info(data));
+getSponsors().then(async data => {
+
+  let output = "";
+
+  data.forEach(entity => {
+
+    const sponsor = entity.sponsorEntity;
+
+    if (sponsor.sponsorshipForViewerAsSponsorable.privacyLevel === "PUBLIC") {
+      const name = sponsor.name || sponsor.login;
+      output += `<a href="${sponsor.url}" title="${name}">\n`;
+      output += `\t<img class="user-icon" src="${sponsor.avatarUrl}" `;
+      output += `alt="${name}" width="100" height="100" />\n`;
+      output += `</a>\n\n`;
+    } else {
+      output += `<img class="user-icon" src="/img/sponsors/user.png" `;
+      output += `alt="Anonymous" width="100" height="100" />\n\n`;
+    }
+
+  });
+
+  const options = {
+    files: TARGET,
+    from: /<!-- SPONSOR START -->.*<!-- SPONSOR END -->/sgm,
+    to: "<!-- SPONSOR START -->\n\n" + output + "<!-- SPONSOR END -->",
+  };
+
+  const results = await replace(options);
+  // console.log("Replacement results:", results[0]);
+
+});
---
+++ website/src/pages/sponsors/index.md
@@ -0,0 +1,52 @@
+# Sponsors
+
+:::tip Consider a sponsorship
+
+Please help me grow and nurture WEBMIDI.js by ❤️ [sponsoring](https://github.com/sponsors/djipco) 
+the project on GitHub.
+
+:::
+
+## About Software Development...
+
+As you surely know, proper software development takes time. This time is spent on coding new 
+features but also on various other important tasks such as writing useful documentation, answering 
+questions, reviewing issues, fixing bugs, writing tests, etc.
+
+While WEBMIDI.js started as a hobby project, it is quietly becoming the "go to" library for MIDI on 
+the web. Hopefully, your contributions will make allow me to continue developing, maintaining and 
+advocating for this project that I cherish. 
+
+Thank you so much. 😀
+
+## Sponsoring Organizations
+---
+
+<a href="https://www.cegepmontpetit.ca/">
+  <img class="logo-cem" src="/img/sponsors/edouard-montpetit-logo.svg" width="150" height="150" />
+</a>
+
+## Sponsors
+---
+
+<!-- SPONSOR START -->
+
+<a href="https://github.com/awatterott" title="Andreas Watterott">
+	<img class="user-icon" src="https://avatars.githubusercontent.com/u/1488433?u=2a498d433dd252a2959c2c846a212624098a03d9&v=4" alt="Andreas Watterott" width="100" height="100" />
+</a>
+
+<a href="https://github.com/rubendax" title="Ruben Dax">
+	<img class="user-icon" src="https://avatars.githubusercontent.com/u/3331057?u=53969596e19481f2c0e49aa7933bc47ccf4f7ce3&v=4" alt="Ruben Dax" width="100" height="100" />
+</a>
+
+<a href="https://github.com/philmillman" title="Phil Miller">
+	<img class="user-icon" src="https://avatars.githubusercontent.com/u/3722211?v=4" alt="Phil Miller" width="100" height="100" />
+</a>
+
+<img class="user-icon" src="/img/sponsors/user.png" alt="Anonymous" width="100" height="100" />
+
+<img class="user-icon" src="/img/sponsors/user.png" alt="Anonymous" width="100" height="100" />
+
+<img class="user-icon" src="/img/sponsors/user.png" alt="Anonymous" width="100" height="100" />
+
+<!-- SPONSOR END -->
