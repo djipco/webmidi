6e868ac0b0b6da07c89be729bf2aa3a7719a9b48
Jean-Philippe Côté <jp@djip.co>
2021-12-14 10:17:01 -0500

    Built on 2021-12-14T10:17:01-05:00

--- dist/esm/webmidi.esm.js
+++ dist/esm/webmidi.esm.js
@@ -17,7 +17,7 @@
  * the License.
  */
 
-/* Version: 3.0.3 - December 14, 2021 10:12:04 */
+/* Version: 3.0.3 - December 14, 2021 10:17:00 */
 /**
  * djipevents v2.0.1
  * https://github.com/djipco/djipevents
