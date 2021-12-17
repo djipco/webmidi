ac627227584db82d1f113ced98b247b4ed3ec87f
Jean-Philippe Côté <jp@djip.co>
2021-12-17 14:09:48 -0500

    Update rollup-plugin-babel

--- scripts/library/rollup.config.cjs.js
+++ scripts/library/rollup.config.cjs.js
@@ -1,4 +1,4 @@
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 import stripCode from "rollup-plugin-strip-code";
 const fs = require("fs");
 const license = require("rollup-plugin-license");
