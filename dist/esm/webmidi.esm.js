a00c680c03097a8c89493751befbf23538477a8f
Jean-Philippe Côté <jp@djip.co>
2021-12-17 14:03:26 -0500

    Built on 2021-12-17T14:03:26-05:00

--- dist/esm/webmidi.esm.js
+++ dist/esm/webmidi.esm.js
@@ -17,7 +17,7 @@
  * the License.
  */
 
-/* Version: 3.0.5 - December 17, 2021 13:19:12 */
+/* Version: 3.0.5 - December 17, 2021 14:03:26 */
 /**
  * The `EventEmitter` class provides methods to implement the _observable_ design pattern. This
  * pattern allows one to _register_ a function to execute when a specific event is _emitted_ by the
