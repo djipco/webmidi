3cae0288dc1aead2096235b17d09ddb0a2e28300
Jean-Philippe Côté <jp@djip.co>
2021-12-17 13:19:13 -0500

    Built on 2021-12-17T13:19:13-05:00

--- dist/esm/webmidi.esm.js
+++ dist/esm/webmidi.esm.js
@@ -1,5 +1,5 @@
 /**
- * WebMidi.js v3.0.4
+ * WebMidi.js v3.0.5
  * A JavaScript library to kickstart your MIDI projects
  * https://webmidijs.org
  * Build generated on December 17th, 2021.
@@ -17,7 +17,7 @@
  * the License.
  */
 
-/* Version: 3.0.4 - December 17, 2021 13:09:13 */
+/* Version: 3.0.5 - December 17, 2021 13:19:12 */
 /**
  * The `EventEmitter` class provides methods to implement the _observable_ design pattern. This
  * pattern allows one to _register_ a function to execute when a specific event is _emitted_ by the
@@ -9768,7 +9768,7 @@ class WebMidi extends EventEmitter {
    * @type string
    */
   get version() {
-    return "3.0.4";
+    return "3.0.5";
   }
 
   /**
