2e31defe73ac59e37a05f68c5ca0a63991030d94
Jean-Philippe Côté <jp@djip.co>
2021-12-20 12:32:23 -0500

    Update

--- examples/typescript/index.ts
+++ examples/typescript/index.ts
@@ -1,11 +1,17 @@
-import { WebMidi } from 'webmidi';
+import { WebMidi } from '../../dist/cjs/webmidi.cjs.js';
 
 async function start() {
 
   await WebMidi.enable();
 
-  console.log(WebMidi.inputs);
-  console.log(WebMidi.outputs);
+  // console.log(WebMidi.inputs);
+  // console.log(WebMidi.outputs);
+
+  const input = WebMidi.getInputByName("MPK mini 3");
+
+  input.addListener('noteon', (e) => {
+    console.log(e);
+  });
 
 }
 
