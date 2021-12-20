12c126ac6919b0bc94b7f8904022bb2d969af43e
Jean-Philippe Côté <jp@djip.co>
2021-12-19 21:06:47 -0500

    Built on 2021-12-19T21:06:47-05:00

--- dist/cjs/webmidi.cjs.js
+++ dist/cjs/webmidi.cjs.js
@@ -17,7 +17,7 @@
  * the License.
  */
 
-/* Version: 3.0.5 - December 19, 2021 21:04:06 */
+/* Version: 3.0.5 - December 19, 2021 21:06:47 */
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
@@ -360,13 +360,13 @@ class EventEmitter {
    * To use more granular options, you must at least define the `event`. Then, you can specify the
    * callback to match or one or more of the additional options.
    *
-   * @param {string} [event=(any events)] The event name.
-   * @param {EventEmitter~callback} [callback=(any callbacks)] Only remove the listeners that match
+   * @param {string} [event] The event name.
+   * @param {EventEmitter~callback} [callback] Only remove the listeners that match
    * this exact callback function.
-   * @param {Object} [options={}]
-   * @param {*} [options.context=(any contexts)] Only remove the listeners that have this exact
+   * @param {Object} [options]
+   * @param {*} [options.context] Only remove the listeners that have this exact
    * context.
-   * @param {number} [options.remaining=(any number)] Only remove the listener if it has exactly
+   * @param {number} [options.remaining] Only remove the listener if it has exactly
    * that many remaining times to be executed.
    */
 
