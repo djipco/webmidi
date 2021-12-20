a20c5e98a6e7445b52fb3fbf7fd62627043cbc0b
Jean-Philippe Côté <jp@djip.co>
2021-12-19 22:11:08 -0500

    Continue example

--- examples/typescript/index.ts
+++ examples/typescript/index.ts
@@ -1,2 +1,12 @@
-import { WebMidi, Input } from 'webmidi';
+import { WebMidi } from 'webmidi';
 
+async function start() {
+
+  await WebMidi.enable();
+
+  console.log(WebMidi.inputs);
+  console.log(WebMidi.outputs);
+
+}
+
+start();
