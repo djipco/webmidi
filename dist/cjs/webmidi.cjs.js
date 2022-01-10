34a5248558e8b828ed3695179ab860a99493ab78
Jean-Philippe Côté <jp@djip.co>
2022-01-10 16:36:54 -0500

    Built on 2022-01-10T16:36:54-05:00

--- dist/cjs/webmidi.cjs.js
+++ dist/cjs/webmidi.cjs.js
@@ -17,7 +17,7 @@
  * the License.
  */
 
-/* Version: 3.0.6 - January 10, 2022 15:35:45 */
+/* Version: 3.0.6 - January 10, 2022 16:36:54 */
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
@@ -74,7 +74,7 @@ class EventEmitter {
    * [`EventEmitter.ANY_EVENT`]{@link #ANY_EVENT} as the first parameter. Note that a global
    * listener will also be triggered by non-registered events.
    *
-   * @param {string|EventEmitter.ANY_EVENT} event The event to listen to.
+   * @param {string|Symbol} event The event to listen to.
    * @param {EventEmitter~callback} callback The callback function to execute when the event occurs.
    * @param {Object} [options={}]
    * @param {Object} [options.context=this] The value of `this` in the callback function.
@@ -123,7 +123,7 @@ class EventEmitter {
    * [`EventEmitter.ANY_EVENT`]{@link EventEmitter#ANY_EVENT} as the first parameter. Note that a
    * global listener will also be triggered by non-registered events.
    *
-   * @param {string|EventEmitter.ANY_EVENT} event The event to listen to
+   * @param {string|Symbol} event The event to listen to
    * @param {EventEmitter~callback} callback The callback function to execute when the event occurs
    * @param {Object} [options={}]
    * @param {Object} [options.context=this] The context to invoke the callback function in.
@@ -169,7 +169,7 @@ class EventEmitter {
    * [`EventEmitter.ANY_EVENT`]{@link EventEmitter#ANY_EVENT}, use
    * [`EventEmitter.ANY_EVENT`]{@link EventEmitter#ANY_EVENT} as the parameter.
    *
-   * @param {string|EventEmitter.ANY_EVENT} [event=(any event)] The event to check
+   * @param {string|Symbol} [event=(any event)] The event to check
    * @param {function|Listener} [callback=(any callback)] The actual function that was added to the
    * event or the {@link Listener} object returned by `addListener()`.
    * @returns {boolean}
@@ -230,7 +230,7 @@ class EventEmitter {
    * events. To get the list of global listeners, specifically use
    * [`EventEmitter.ANY_EVENT`]{@link EventEmitter#ANY_EVENT} as the parameter.
    *
-   * @param {string|EventEmitter.ANY_EVENT} event The event to get listeners for.
+   * @param {string|Symbol} event The event to get listeners for.
    * @returns {Listener[]} An array of [`Listener`]{@link Listener} objects.
    */
 
@@ -250,8 +250,8 @@ class EventEmitter {
    * listeners alone. If you truly want to suspends all callbacks for a specific
    * [`EventEmitter`]{@link EventEmitter}, simply set its `eventsSuspended` property to `true`.
    *
-   * @param {string|EventEmitter.ANY_EVENT} event The event for which to suspend execution of all
-   * callback functions.
+   * @param {string|Symbol} event The event name (or `EventEmitter.ANY_EVENT`) for which to suspend
+   * execution of all callback functions.
    */
 
 
@@ -271,8 +271,8 @@ class EventEmitter {
    * counter-intuitive, it allows the selective unsuspension of global listeners while leaving other
    * callbacks alone.
    *
-   * @param {string|EventEmitter.ANY_EVENT} event The event for which to resume execution of all
-   * callback functions.
+   * @param {string|Symbol} event The event name (or `EventEmitter.ANY_EVENT`) for which to resume
+   * execution of all callback functions.
    */
 
 
@@ -289,8 +289,8 @@ class EventEmitter {
    * number for a "regular" event. To get the number of global listeners, specifically use
    * [`EventEmitter.ANY_EVENT`]{@link EventEmitter#ANY_EVENT} as the parameter.
    *
-   * @param {string|EventEmitter.ANY_EVENT} event The event which is usually a string but can also
-   * be the special [`EventEmitter.ANY_EVENT`]{@link EventEmitter#ANY_EVENT} symbol.
+   * @param {string|Symbol} event The event which is usually a string but can also be the special
+   * [`EventEmitter.ANY_EVENT`]{@link EventEmitter#ANY_EVENT} symbol.
    * @returns {number} An integer representing the number of listeners registered for the specified
    * event.
    */
@@ -360,15 +360,13 @@ class EventEmitter {
    * To use more granular options, you must at least define the `event`. Then, you can specify the
    * callback to match or one or more of the additional options.
    *
-   * @method
    * @param {string} [event] The event name.
-   * @param {EventEmitter~callback} [callback] Only remove the listeners that match
-   * this exact callback function.
+   * @param {EventEmitter~callback} [callback] Only remove the listeners that match this exact
+   * callback function.
    * @param {Object} [options]
-   * @param {*} [options.context] Only remove the listeners that have this exact
-   * context.
-   * @param {number} [options.remaining] Only remove the listener if it has exactly
-   * that many remaining times to be executed.
+   * @param {*} [options.context] Only remove the listeners that have this exact context.
+   * @param {number} [options.remaining] Only remove the listener if it has exactly that many
+   * remaining times to be executed.
    */
 
 
@@ -402,7 +400,7 @@ class EventEmitter {
    * duration, the promise is rejected. This makes it super easy to wait for an event and timeout
    * after a certain time if the event is not triggered.
    *
-   * @param {string|EventEmitter.ANY_EVENT} event The event to wait for
+   * @param {string|Symbol} event The event to wait for
    * @param {Object} [options={}]
    * @param {number} [options.duration=Infinity] The number of milliseconds to wait before the
    * promise is automatically rejected.
@@ -451,7 +449,7 @@ class EventEmitter {
  * contextual information such as the event being listened to, the object the listener was attached
  * to, the callback function and so on.
  *
- * @param {string|EventEmitter.ANY_EVENT} event The event being listened to
+ * @param {string|Symbol} event The event being listened to
  * @param {EventEmitter} target The [`EventEmitter`]{@link EventEmitter} object that the listener is
  * attached to.
  * @param {EventEmitter~callback} callback The function to call when the listener is triggered
@@ -2012,7 +2010,7 @@ class OutputChannel extends EventEmitter {
    * aftertouch. For a channel-wide aftertouch message, use
    * [`sendChannelAftertouch()`]{@link #sendChannelAftertouch}.
    *
-   * @param note {number|Note|string|number[]|Note[]|string[]} The note(s) for which you are sending
+   * @param target {number|Note|string|number[]|Note[]|string[]} The note(s) for which you are sending
    * an aftertouch value. The notes can be specified by using a MIDI note number (`0` - `127`), a
    * [`Note`](Note) object, a note identifier (e.g. `C3`, `G#4`, `F-1`, `Db7`) or an array of the
    * previous types. When using a note identifier, octave range must be between `-1` and `9`. The
@@ -6889,7 +6887,7 @@ class InputChannel extends EventEmitter {
    * If a  is passed in, the method will take into account any [`octaveOffset`](#octaveOffset)
    * defined.
    *
-   * @param [input] {number|string|Note} The note to get the state for. The
+   * @param {number|string|Note} note The note to get the state for. The
    * [`octaveOffset`](#octaveOffset) will be factored in for note identifiers and
    * [`Note`]{@link Note} objects.
    * @returns {boolean}
