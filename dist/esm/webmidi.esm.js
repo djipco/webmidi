f63bbf07348ef0bc2fa5c48a9a2bbe2d87e61cf7
Jean-Philippe Côté <jp@djip.co>
2022-01-10 20:53:50 -0500

    Built on 2022-01-10T20:53:50-05:00

--- dist/esm/webmidi.esm.js
+++ dist/esm/webmidi.esm.js
@@ -17,7 +17,7 @@
  * the License.
  */
 
-/* Version: 3.0.6 - January 10, 2022 20:46:03 */
+/* Version: 3.0.6 - January 10, 2022 20:53:50 */
 /**
  * The `EventEmitter` class provides methods to implement the _observable_ design pattern. This
  * pattern allows one to _register_ a function to execute when a specific event is _emitted_ by the
@@ -8781,6 +8781,25 @@ class Input extends EventEmitter {
  * @since 2.1
  */
 
+/*START-ESM*/
+
+// If this code is running under Node.js in "module" mode (because "type": "module" is used in
+// the package.json file), then we must import the `jzz` module. This import attempt will fail
+// in the browser, which is what we want (hence the empty catch clause). This block is stripped
+// out in the IIFE and CJS versions where it isn't needed.
+// try {
+//   const jzz = await import("jzz");
+//   global["navigator"] = jzz.default;
+//   // eslint-disable-next-line no-empty
+// } catch (err) {}
+
+if (typeof window === "undefined") {
+  eval('import {jzz} from "jzz"'); // This hides the import from Webpack (I feel dirty).
+  global["navigator"] = jzz;
+}
+
+/*END-ESM*/
+
 /**
  * The `WebMidi` object makes it easier to work with the low-level Web MIDI API. Basically, it
  * simplifies sending outgoing MIDI messages and reacting to incoming MIDI messages.
@@ -8974,19 +8993,7 @@ class WebMidi extends EventEmitter {
    */
   async enable(options = {}, legacy = false) {
 
-    /*START-ESM*/
 
-    // If this code is running under Node.js in "module" mode (because "type": "module" is used in
-    // the package.json file), then we must import the `jzz` module. This import attempt will fail
-    // in the browser, which is what we want (hence the empty catch clause). This block is stripped
-    // out in the IIFE and CJS versions where it isn't needed.
-    try {
-      const jzz = await import('jzz');
-      global["navigator"] = jzz.default;
-      // eslint-disable-next-line no-empty
-    } catch (err) {}
-
-    /*END-ESM*/
 
     this.validation = (options.validation !== false);
 
