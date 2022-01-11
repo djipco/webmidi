965621921f355fa9793c89f59a9bf582546acdb7
Jean-Philippe Côté <jp@djip.co>
2022-01-10 20:41:19 -0500

    Built on 2022-01-10T20:41:19-05:00

--- dist/cjs/webmidi.cjs.js
+++ dist/cjs/webmidi.cjs.js
@@ -17,7 +17,7 @@
  * the License.
  */
 
-/* Version: 3.0.6 - January 10, 2022 20:40:45 */
+/* Version: 3.0.6 - January 10, 2022 20:41:19 */
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
@@ -8272,8 +8272,8 @@ class Input extends EventEmitter {
 // }
 
 if (typeof window === "undefined") {
-  let jzz;
-  eval('jzz = require("jzz")');
+  let jzz; // eval('jzz = require("jzz")');
+
   global["navigator"] = jzz;
 }
 /*END-CJS*/
