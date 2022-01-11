a6d5e959cfc5f22e429cc8d79f39f67349bf07f7
Jean-Philippe Côté <jp@djip.co>
2022-01-11 10:27:45 -0500

    Generated by TypeScript compiler on 2022-01-11T10:27:45-05:00

--- dist/cjs/webmidi.cjs.d.ts
+++ dist/cjs/webmidi.cjs.d.ts
@@ -4316,7 +4316,16 @@ export class Utilities {
      * @since 2.0.0
      */
     static getChannelModeByNumber(number: number): string | false;
-    static isNode(): any;
+    /**
+     * Indicates whether the execution environment is Node.js (`true`) or not (`false`)
+     * @type {boolean}
+     */
+    static get isNode(): boolean;
+    /**
+     * Indicates whether the execution environment is a browser (`true`) or not (`false`)
+     * @type {boolean}
+     */
+    static get isBrowser(): boolean;
 }
 declare const wm: WebMidi;
 /**
