cbdf66bebfa1773077903b8892fa998d0cf80581
Jean-Philippe Côté <jp@djip.co>
2022-04-01 09:32:59 -0400

    Built on 2022-04-01T09:32:59-04:00

--- dist/cjs/webmidi.cjs.js
+++ dist/cjs/webmidi.cjs.js
@@ -1,8 +1,8 @@
 /**
- * WEBMIDI.js v3.0.15
+ * WEBMIDI.js v3.0.16
  * A JavaScript library to kickstart your MIDI projects
  * https://webmidijs.org
- * Build generated on March 31st, 2022.
+ * Build generated on April 1st, 2022.
  *
  * © Copyright 2015-2022, Jean-Philippe Côté.
  *
@@ -17,7 +17,7 @@
  * the License.
  */
 
-/* Version: 3.0.15 - March 31, 2022 15:20:30 */
+/* Version: 3.0.16 - April 1, 2022 09:32:58 */
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
@@ -9368,7 +9368,7 @@ class WebMidi extends EventEmitter {
 
 
   get version() {
-    return "3.0.15";
+    return "3.0.16";
   }
   /**
    * @private
