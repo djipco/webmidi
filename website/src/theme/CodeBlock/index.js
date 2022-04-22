609559cd37c7133a15d7d897d382b3a2dcaa7c77
Jean-Philippe Côté <jp@djip.co>
2022-04-22 11:15:04 -0400

    Re-swizzle NavBar

---
+++ website/src/theme/CodeBlock/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CodeBlock from '@theme-original/CodeBlock';
+
+export default function CodeBlockWrapper(props) {
+  return (
+    <>
+      <CodeBlock {...props} />
+    </>
+  );
+}
